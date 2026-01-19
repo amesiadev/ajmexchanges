2ba904bdec4dd77ca52bd0ab5507e9ef1dc63ac7
amesiadev <73813630+amesiadev@users.noreply.github.com>
2026-01-19 11:23:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ let bcvRate = null;
 let eurRate = null;
 let usdToCopRate = null;
 let usdRate  = null;
-let tasa = 400;
-let tasaVenta = 520;
+let tasa = 380;
+let tasaVenta = 430;
 
 // Obtener tasas
 async function getRates() {
