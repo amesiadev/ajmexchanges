442378114f5ce60c7d9fb0e953b534a3a671a56d
amesiadev <73813630+amesiadev@users.noreply.github.com>
2026-03-13 08:23:53 -0500

    Update tasa and tasaVenta values

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ let bcvRate = null;
 let eurRate = null;
 let usdToCopRate = null;
 let usdRate  = null;
-let tasa = 540;
-let tasaVenta = 630;
+let tasa = 550;
+let tasaVenta = 620;
 
 // Obtener tasas
 async function getRates() {
