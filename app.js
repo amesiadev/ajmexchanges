1e2bd54dad8269fe47a177f20cffd752db9c811a
amesiadev <73813630+amesiadev@users.noreply.github.com>
2026-06-27 06:51:00 -0500

    Update tasa and tasaVenta values in app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ let eurRate = null;
 let usdToCopRate = null;
 let usdRate  = null;
 let spreadRate = 100;
-let tasa = 710;
-let tasaVenta = 816;
+let tasa = 680;
+let tasaVenta = 800;
 let rate_bcv_incr = 7;
 
 function formatNumber(value) {
