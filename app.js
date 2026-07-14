9c1ba6790269d3f35b39bf90b87a972a9752f95d
amesiadev <73813630+amesiadev@users.noreply.github.com>
2026-07-14 08:55:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ let eurRate = null;
 let usdToCopRate = null;
 let usdRate  = null;
 let spreadRate = 100;
-let tasa = 740;
-let tasaVenta = 830;
+let tasa = 765;
+let tasaVenta = 850;
 let rate_bcv_incr = 7;
 
 function formatNumber(value) {
